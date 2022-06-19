b6c63f2297c3d33f7c6b10f4a919610d459bd83c
Sai Kranthi <iamsaikranthi@gmail.com>
2022-06-20 04:33:37 +0530

    feat: add wave-portal smartcontract setup

---
+++ scripts/run.js
@@ -0,0 +1,33 @@
+const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
+  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+
+  console.log('Contract deployed to:', waveContract.address);
+  console.log('Contract deployed by:', owner.address);
+
+  const waveCount = await waveContract.getTotalWaves();
+  console.log(waveCount.toNumber());
+
+  let waveTxn = await waveContract.wave('A message!');
+  await waveTxn.wait();
+
+  waveTxn = await waveContract.connect(randomPerson).wave('Another message!');
+  await waveTxn.wait(); // Wait for the transaction
+
+  const allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
